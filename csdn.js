5b4e831ba24cedfb9a626d476dfa534bd3c24185
Qun Sun <qsun@esri.com>
2016-08-12 18:39:20 +0800

    update csdn.js

--- csdn.js
+++ csdn.js
@@ -16,19 +16,19 @@
   }
 
   function createRepoNode(repoInfo, parentNode) {
-    var str = '<div style="color:#3d84b0;padding:15px;">' +
+    var str = '<a style="color:#3d84b0;padding:15px;text-decoration:none;" target="_blank">' +
       '<div>' +
-        '<a style="display:inline-block;height:24px;line-height:24px;vertical-align:middle;text-decoration:none;color:#666;"></a>' +
-        '<span style="display:inline-block;height:24px;line-height24px;vertical-align:middle;width:24px;margin-left:10px;background:url(http://www.easyicon.net/api/resizeApi.php?id=1171410&size=16) no-repeat center"></span>' +
-        '<span class="count" style="display:inline-block;height:24px;line-height24px;vertical-align:middle;width:24px;margin-left:5px;"></span>' +
+        '<span class="repo-name" style="display:inline-block;height:24px;line-height:24px;vertical-align:middle;"></span>' +
+        '<span class="star-icon" style="display:inline-block;height:24px;line-height24px;vertical-align:middle;width:24px;margin-left:10px;background:url(http://www.easyicon.net/api/resizeApi.php?id=1171410&size=16) no-repeat center"></span>' +
+        '<span class="star-count" style="height:24px;line-height24px;vertical-align:middle;width:24px;margin-left:5px;"></span>' +
       '</div>' +
-      '<div class="description"></div>' +
-    '</div>';
+      '<div class="repo-description"></div>' +
+    '</a>';
     var a = $(str);
-    a.attr("title", repoInfo.description);
-    $("a", a).text(repoInfo.name).attr("href", repoInfo.html_url);
-    $(".count", a).text(repoInfo.stargazers_count);
-    $(".description", a).text(repoInfo.description);
+    a.attr("href", repoInfo.html_url);//attr("title", repoInfo.description)
+    $(".repo-name", a).text(repoInfo.name);
+    $(".star-count", a).text(repoInfo.stargazers_count);
+    $(".repo-description", a).text(repoInfo.description);
     a.appendTo(parentNode);
   }
 
