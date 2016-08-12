ec0da211c52597f1ebf1c5fb58d64b93d37d85bc
Qun Sun <qsun@esri.com>
2016-08-12 18:23:00 +0800

    update csdn.js

--- csdn.js
+++ csdn.js
@@ -16,12 +16,19 @@
   }
 
   function createRepoNode(repoInfo, parentNode) {
-    var str = '<div>' +
-      '<a style="display:inline-block;height:24px;line-height:24px;vertical-align:middle;text-decoration:none;color:#666;"></a>' +
-      '<span style="display:inline-block;height:24px;line-height24px;vertical-align:middle;width:24px;margin-left:10px;background: "></span>' +
-      '</div>';
+    var str = '<div style="color:#3d84b0;padding:15px;">' +
+      '<div>' +
+        '<a style="display:inline-block;height:24px;line-height:24px;vertical-align:middle;text-decoration:none;color:#666;"></a>' +
+        '<span style="display:inline-block;height:24px;line-height24px;vertical-align:middle;width:24px;margin-left:10px;background:url(http://www.easyicon.net/api/resizeApi.php?id=1171410&size=16) no-repeat center"></span>' +
+        '<span class="count" style="display:inline-block;height:24px;line-height24px;vertical-align:middle;width:24px;margin-left:5px;"></span>' +
+      '</div>' +
+      '<div class="description"></div>' +
+    '</div>';
     var a = $(str);
-    $("a", a).text(repoInfo.name).attr("href", repoInfo.html_url).attr("title", repoInfo.description);
+    a.attr("title", repoInfo.description);
+    $("a", a).text(repoInfo.name).attr("href", repoInfo.html_url);
+    $(".count", a).text(repoInfo.stargazers_count);
+    $(".description", a).text(repoInfo.description);
     a.appendTo(parentNode);
   }
 
