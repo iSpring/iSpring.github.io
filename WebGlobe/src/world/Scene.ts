04a5243c9e59133910d0e95ef7988d5fd57a1360
iSpring <QSun@esri.com>
2016-10-30 22:34:46 +0800

    update WebGlobe

--- WebGlobe/src/world/Scene.ts
+++
@@ -1,9 +0,0 @@
-///<amd-module name="world/Scene"/>
-import Object3DComponents = require('./Object3DComponents');
-import TiledLayer = require("./TiledLayer");
-
-class Scene extends Object3DComponents{
-    tiledLayer: TiledLayer;
-}
-
-export = Scene;
