14c6cbae064a80e5568b8c7409a5ed786418f84f
iSpring <QSun@esri.com>
2017-01-31 07:51:46 +0800

    update csdn.js

--- csdn.js
+++ csdn.js
@@ -1,48 +1,54 @@
 (function() {
-  var containerNode = document.getElementById("custom_column_21591691");
-  if (!containerNode) {
-    return;
-  }
-
-  var repoNames = ["WebGlobe", "GamePlane"];
-
-  function getRepoInfo(data, repoName) {
-    for (var i = 0; i < data.length; i++) {
-      if (data[i].name === repoName) {
-        return data[i];
-      }
+    var containerNode = document.getElementById("custom_column_21591691");
+    if (!containerNode) {
+        return;
     }
-    return null;
-  }
 
-  function createRepoNode(repoInfo, parentNode) {
-    //https://img.shields.io/github/stars/iSpring/WebGlobe.svg
-    var starBadgeUrl = "https://img.shields.io/github/stars/iSpring/" + repoInfo.name + ".svg";
-    var str = '<a style="display:inline-block;color:#3d84b0;padding:15px;text-decoration:none;" target="_blank">' +
-      '<div>' +
-        '<span class="repo-name" style="display:inline-block;height:24px;line-height:24px;vertical-align:middle;"></span>' +
-        '<img class="star-badge" style="height:20px;vertical-align:middle;margin-left:20px;" src="' + starBadgeUrl + '" />' +
-      '</div>' +
-      '<div class="repo-description"></div>' +
-    '</a>';
-    var a = $(str);
-    a.attr("href", repoInfo.html_url);
-    $(".repo-name", a).text(repoInfo.name);
-    $(".repo-description", a).text(repoInfo.description);
-    a.appendTo(parentNode);
-  }
+    var panelBody = $(".panel_body", containerNode)[0];
 
-  $.ajax("https://api.github.com/users/iSpring/repos", {
-    dataType: 'json',
-    data: {},
-    complete: function(response) {
-      var data = response.responseJSON;
-      $.each(repoNames, function(i, repoName) {
-        var repoInfo = getRepoInfo(data, repoName);
-        if (repoInfo) {
-          createRepoNode(repoInfo, containerNode);
+    if (panelBody) {
+        panelBody.style.textAlign = "center";
+    }
+
+    var repoNames = ["WebGlobe", "GamePlane"];
+
+    function getRepoInfo(data, repoName) {
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].name === repoName) {
+                return data[i];
+            }
         }
-      });
+        return null;
     }
-  });
+
+    function createRepoNode(repoInfo, parentNode) {
+        //https://img.shields.io/github/stars/iSpring/WebGlobe.svg
+        var starBadgeUrl = "https://img.shields.io/github/stars/iSpring/" + repoInfo.name + ".svg";
+        var str = '<a style="display:inline-block;color:#3d84b0;padding:15px;text-decoration:none;" target="_blank">' +
+            '<div>' +
+            '<span class="repo-name" style="display:inline-block;height:24px;line-height:24px;vertical-align:middle;"></span>' +
+            '<img class="star-badge" style="height:20px;vertical-align:middle;margin-left:20px;" src="' + starBadgeUrl + '" />' +
+            '</div>' +
+            '<div class="repo-description"></div>' +
+            '</a>';
+        var a = $(str);
+        a.attr("href", repoInfo.html_url);
+        $(".repo-name", a).text(repoInfo.name);
+        $(".repo-description", a).text(repoInfo.description);
+        a.appendTo(parentNode);
+    }
+
+    $.ajax("https://api.github.com/users/iSpring/repos", {
+        dataType: 'json',
+        data: {},
+        complete: function(response) {
+            var data = response.responseJSON;
+            $.each(repoNames, function(i, repoName) {
+                var repoInfo = getRepoInfo(data, repoName);
+                if (repoInfo) {
+                    createRepoNode(repoInfo, containerNode);
+                }
+            });
+        }
+    });
 })();
