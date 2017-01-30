4e748db40eafcbf1834ca54c192d76101901ccec
iSpring <QSun@esri.com>
2017-01-30 18:29:19 +0800

    update csdn.js to use GitHub star badge from img.shields.io

--- csdn.js
+++ csdn.js
@@ -15,19 +15,24 @@
     return null;
   }
 
+  //https://img.shields.io/github/stars/iSpring/WebGlobe.svg
+  //https://github.com/iSpring/WebGlobe/stargazers
+
   function createRepoNode(repoInfo, parentNode) {
+    var starBadgeUrl = "https://img.shields.io/github/stars/iSpring/" + repoInfo.name + ".svg";
     var str = '<a style="display:inline-block;color:#3d84b0;padding:15px;text-decoration:none;" target="_blank">' +
       '<div>' +
         '<span class="repo-name" style="display:inline-block;height:24px;line-height:24px;vertical-align:middle;"></span>' +
-        '<span class="star-icon" style="display:inline-block;height:24px;line-height24px;vertical-align:middle;width:11px;margin-left:10px;background:url(http://www.easyicon.net/api/resizeApi.php?id=1171410&size=16) no-repeat center;background-size:contain;"></span>' +
-        '<span class="star-count" style="height:24px;line-height24px;vertical-align:middle;width:24px;margin-left:5px;"></span>' +
+        // '<span class="star-icon" style="display:inline-block;height:24px;line-height24px;vertical-align:middle;width:11px;margin-left:10px;background:url(http://www.easyicon.net/api/resizeApi.php?id=1171410&size=16) no-repeat center;background-size:contain;"></span>' +
+        //'<span class="star-count" style="height:24px;line-height24px;vertical-align:middle;width:24px;margin-left:5px;"></span>' +
+        '<img class="star-badge" style="height:24px;line-height24px;vertical-align:middle;width:24px;margin-left:5px;" url="' + starBadgeUrl + '" />' +
       '</div>' +
       '<div class="repo-description"></div>' +
     '</a>';
     var a = $(str);
-    a.attr("href", repoInfo.html_url);//attr("title", repoInfo.description)
+    a.attr("href", repoInfo.html_url);
     $(".repo-name", a).text(repoInfo.name);
-    $(".star-count", a).text(repoInfo.stargazers_count);
+    // $(".star-count", a).text(repoInfo.stargazers_count);
     $(".repo-description", a).text(repoInfo.description);
     a.appendTo(parentNode);
   }
