1ed355a6f4befe0545301419fd6fcee1b7682fe4
iSpring <QSun@esri.com>
2017-01-30 18:37:08 +0800

    update csdn.js to use GitHub star badge from img.shields.io

--- csdn.js
+++ csdn.js
@@ -15,24 +15,19 @@
     return null;
   }
 
-  //https://img.shields.io/github/stars/iSpring/WebGlobe.svg
-  //https://github.com/iSpring/WebGlobe/stargazers
-
   function createRepoNode(repoInfo, parentNode) {
+    //https://img.shields.io/github/stars/iSpring/WebGlobe.svg
     var starBadgeUrl = "https://img.shields.io/github/stars/iSpring/" + repoInfo.name + ".svg";
     var str = '<a style="display:inline-block;color:#3d84b0;padding:15px;text-decoration:none;" target="_blank">' +
       '<div>' +
         '<span class="repo-name" style="display:inline-block;height:24px;line-height:24px;vertical-align:middle;"></span>' +
-        // '<span class="star-icon" style="display:inline-block;height:24px;line-height24px;vertical-align:middle;width:11px;margin-left:10px;background:url(http://www.easyicon.net/api/resizeApi.php?id=1171410&size=16) no-repeat center;background-size:contain;"></span>' +
-        //'<span class="star-count" style="height:24px;line-height24px;vertical-align:middle;width:24px;margin-left:5px;"></span>' +
-        '<img class="star-badge" style="height:24px;line-height24px;vertical-align:middle;margin-left:5px;" src="' + starBadgeUrl + '" />' +
+        '<img class="star-badge" style="height:20px;vertical-align:middle;margin-left:20px;" src="' + starBadgeUrl + '" />' +
       '</div>' +
       '<div class="repo-description"></div>' +
     '</a>';
     var a = $(str);
     a.attr("href", repoInfo.html_url);
     $(".repo-name", a).text(repoInfo.name);
-    // $(".star-count", a).text(repoInfo.stargazers_count);
     $(".repo-description", a).text(repoInfo.description);
     a.appendTo(parentNode);
   }
