6d574bb21e2922cce11ee6ab943f06c033af1708
Qun Sun <qsun@esri.com>
2017-02-10 14:11:28 +0800

    update csdn.js

--- csdn.js
+++ csdn.js
@@ -1,4 +1,17 @@
 (function() {
+    function forkMeOnGitHub(){
+        //https://github.com/blog/273-github-ribbons
+        //http://tholman.com/github-corners/
+        var a = document.createElement("a");
+        a.style.cssText = "position:absolute;right:0;top:0;";
+        a.target = "_blank";
+        a.href = "https://github.com/iSpring/";
+        a.innerHTML = '<img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png">';
+        document.body.append(a);
+    }
+
+    window.forkMeOnGitHub = forkMeOnGitHub;
+
     var containerNode = document.getElementById("custom_column_21591691");
     if (!containerNode) {
         return;
@@ -25,7 +38,7 @@
         //https://img.shields.io/github/stars/iSpring/WebGlobe.svg
         // var starBadgeUrl = "https://img.shields.io/github/stars/iSpring/" + repoInfo.name + ".svg";
 
-        //https://ghbtns.com/github-btn.html?user=iSpring&repo=WebGlobe&type=star&count=true        
+        //https://ghbtns.com/github-btn.html?user=iSpring&repo=WebGlobe&type=star&count=true
         var starBadgeUrl = 'https://ghbtns.com/github-btn.html?user=iSpring&repo=' + repoInfo.name + '&type=star&count=true';
 
         var str = '' +
