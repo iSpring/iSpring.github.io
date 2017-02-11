d61eb5c6d89c12dcb52a7c8d0cf44c8728cf5b52
iSpring <QSun@esri.com>
2017-02-11 12:18:36 +0800

    update csdn.js

--- csdn.js
+++ csdn.js
@@ -1,9 +1,9 @@
 (function() {
-    function forkMeOnGitHub(){
+    function forkMeOnGitHub() {
         //https://github.com/blog/273-github-ribbons
         //http://tholman.com/github-corners/
         var a = document.createElement("a");
-        a.style.cssText = "position:fixed;right:0;top:0;";
+        a.style.cssText = "position:fixed;right:0;top:0;z-index:1002;";
         a.target = "_blank";
         a.href = "https://github.com/iSpring/";
         a.innerHTML = '<img style="position:absolute; top:0; right:0; left:auto; bottom:auto; border:0;" src="https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png">';
