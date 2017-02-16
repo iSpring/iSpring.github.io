71d5d284c348f9101c02fe20af79a4287d6d0bc8
Qun Sun <qsun@esri.com>
2017-02-16 16:39:45 +0800

    update es6-tutorials

--- es6-tutorials/let-and-const/index.js
+++ es6-tutorials/let-and-const/index.js
@@ -9,22 +9,24 @@ f("aa","bb");
 //output: ["aa", "bb"]
 
 -------------------------------
-
+//variable has function scope
+//1.
 function f(){
   console.log(a);
 }
 f();
 //Uncaught ReferenceError: a is not defined
 
+//2.
 function f(){
-  console.log("a:", a);
-  var a = "abc";
+  console.log(a);
+  var a = "local a";
+  console.log(a);
 }
 f();
-//output: a: undefined
+//output: undefined   local a
 
--------------------------------
-
+//3.
 function f(){
   console.log("a:", a);
   if(false){
@@ -35,3 +37,51 @@ f();
 //output: a: undefined
 
 -------------------------------
+//scope chain
+//1.
+var a = "a";
+function f(){
+  console.log(a);
+}
+f();
+//output: a
+
+//2.
+var a = "a";
+function f(){
+  var a = "local a";
+  console.log(a);
+}
+//output: local a
+
+//3.
+var a = "a";
+function f(){
+  console.log(a);
+  var a = "local a";
+  console.log(a);
+}
+f();
+//output: undefined  local a
+
+//4.
+var a = "a";
+function f(){
+  console.log(a);
+  if(false){
+    var a = "local a";
+  }
+}
+f();
+//output: undefined
+
+//5.
+var a = "a";
+function f(){
+  var a = "aa";
+  return function(){
+    console.log(a);
+  }
+}
+f()();
+//output: aa
