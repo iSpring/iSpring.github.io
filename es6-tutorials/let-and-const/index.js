43303180d97ad680448cc70f1417fcd916088ce5
Qun Sun <qsun@esri.com>
2017-02-16 15:39:47 +0800

    add es6-tutorials

---
+++ es6-tutorials/let-and-const/index.js
@@ -0,0 +1,37 @@
+﻿
+function a(){
+  return function(){
+    console.log(arguments);
+  }
+}
+f = a(1,2,3);
+f("aa","bb");
+//output: ["aa", "bb"]
+
+-------------------------------
+
+function f(){
+  console.log(a);
+}
+f();
+//Uncaught ReferenceError: a is not defined
+
+function f(){
+  console.log("a:", a);
+  var a = "abc";
+}
+f();
+//output: a: undefined
+
+-------------------------------
+
+function f(){
+  console.log("a:", a);
+  if(false){
+    var a = "abc";
+  }
+}
+f();
+//output: a: undefined
+
+-------------------------------
