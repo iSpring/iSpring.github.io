884b34c5fb747c6147b00f5cfde61409fe2cc4fb
qun <qsun@esri.com>
2017-08-09 22:30:36 +0800

    update

---
+++ node/dgram/client.js
@@ -0,0 +1,7 @@
+//https://nodejs.org/api/dgram.html
+const dgram = require('dgram');
+const message = Buffer.from('Some bytes');
+const client = dgram.createSocket('udp4');
+client.send(message, 41234, 'localhost', (err) => {
+  client.close();
+});
